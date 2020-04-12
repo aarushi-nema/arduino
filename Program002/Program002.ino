2858b056dd318424c21e6ed34c655322727cfb5c
aarushi-nema <aarushi.nema@gmail.com>
2020-04-12 19:26:15 +0530

    Program002

---
+++ Program002/Program002.ino
@@ -0,0 +1,10 @@
+//turn LED (on arduino) OFF
+void setup() {
+  pinMode(13,OUTPUT);// put your setup code here, to run once:
+
+}
+
+void loop() {
+   digitalWrite(13,LOW);// put your main code here, to run repeatedly:
+
+}
